19fadb62d909e39901e9e561864295ee12e90565
Arwalk <SirAgentProvocateurArwalk@gmail.com>
2026-02-15 10:29:48 +0100

    build(draft): base of building using zig.build done

---
+++ build.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+fn installTheme(b: *std.Build, name: []const u8) void {
+    const dep = b.dependency(name, .{});
+    b.installDirectory(.{ .source_dir = dep.path(""), .install_dir = .{ .custom = "../themes" }, .install_subdir = name });
+}
+
+pub fn build(b: *std.Build) void {
+    installTheme(b, "ananke");
+
+    const hugo_server = b.addSystemCommand(&.{ "hugo", "server" });
+
+    const server = b.step("server", "run hugo server");
+    server.dependOn(&hugo_server.step);
+}
